3f4dcc528ebe7d4f3f7381595b8f3a75cd47c876
jooijin <48429380+jooijin@users.noreply.github.com>
2021-03-06 16:36:30 +0900

    Update 주식가격.cpp
    필요없는 코드 제거
    코드 길이 줄임

--- programmers/주식가격.cpp
+++ programmers/주식가격.cpp
@@ -1,36 +1,26 @@
 #include <string>
 #include <vector>
 
+// 이중 for문은 비효율적인거같다,, 다른 방법 없을까?
+// 큐/스택 문제인데 큐와 스택을 사용하지 않은 것도 문제이다.
+// 나중에 좀 더 개선해야겠다.
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
     vector<int> answer;
-    int temp;
     
-    for (int i=0; i<prices.size()-1; i++)
-    {
-        temp=0;
-        for (int j=i+1; j<prices.size(); j++)
-        {
-            //if (prices[i]<=prices[j]) -> 쓸모없는 코드 쓰지말자
-            
-            temp++;
-            
-            if (prices[i]>prices[j] || j == prices.size()-1)
-            {
-                answer.push_back(temp);
+    for(int i=0; i<prices.size(); i++) {
+        int temp = prices[i];
+        for(int j=i; j<prices.size(); j++) {
+            if(prices[j]<temp) {
+                answer.push_back(j-i);
                 break;
             }
-            
-            /*if(j == prices.size()-1)
-            {
-                answer.push_back(temp);
-                break;
-            }*/
+        }
+        if(answer.size() == i) {
+            answer.push_back(prices.size()-i-1);
         }
     }
-    
-    answer.push_back(0);
     
     return answer;
 }
