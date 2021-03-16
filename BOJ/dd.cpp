3810925d13242a0980719a4000db60ff0a664b1e
jooijin <48429380+jooijin@users.noreply.github.com>
2021-03-16 23:59:39 +0900

    Create dd.cpp

---
+++ BOJ/dd.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+bool compare(pair<int,int> a, pair<int,int> b) {
+	if(a.second<b.second) {
+		return true;
+	}
+	
+	return false;
+}
+
+int main() {
+	int N, now;
+	int ans = 1;
+	vector<pair<int, int> > time;
+	
+	cin>>N;
+	
+	for(int i=0; i<N; i++) {
+		int tmp1,tmp2;
+		cin>>tmp1>>tmp2;
+		time.push_back(make_pair(tmp1, tmp2));
+	}
+	
+	sort(time.begin(), time.end(), compare);
+	
+	now = time[0].second;
+	
+	for(int i=1; i<N; i++) {
+		if(time[i].first>=now) {
+			ans++;
+			now = time[i].second;
+		}
+	}
+	
+	cout<<ans;
+	
+	return 0;
+	
+}
