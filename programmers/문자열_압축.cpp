dd831962e7179639c8a6d8027069d52b471d63c3
jooijin <48429380+jooijin@users.noreply.github.com>
2021-03-31 23:38:48 +0900

    Create 문자열_압축.cpp

---
+++ programmers/문자열_압축.cpp
@@ -0,0 +1,31 @@
+// 미완성..시간초과
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = s.size();
+    int len = s.size();
+    int cnt = 0;
+    int temp;
+    
+    for(int i=1; i<s.size()/2; i++){
+        temp = len;
+        for(int j=0; j+2*i<s.size(); j+i) {
+            cnt = 1;
+            if(s.substr(j,i) == s.substr(j+i,i)){
+                for(int k=j+i; k+2*i<s.size() && s.substr(k,i) == s.substr(k+i,i); k+i) {
+                    cnt++;
+                    j+i;
+                }
+            }
+            temp = temp-i*cnt+i+cnt/10+1;
+        }
+        answer = min(answer, temp);
+    }
+    
+    return answer;
+}
